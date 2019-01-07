434a82a0898847a75bf896f829a9d8d0aba72260
Jonathan Luk <jluk87@gmail.com>
2019-01-06 19:18:56 -0500

    configured ApplicationController, can shotgun and view the index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < Sinatra::Base
 
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+  end
+
   get '/' do
     erb :index
   end
+
 end
