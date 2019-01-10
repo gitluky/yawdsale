681345a6546bcab9cefaac56f36ed0dafcd86c6c
Jonathan Luk <jluk87@gmail.com>
2019-01-09 22:55:23 -0500

    chalked out most of the sign up and login routes, needs to be tested

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
+require 'rack-flash'
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
+    use Rack::Flash
   end
 
   get '/' do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,41 @@ class UsersController < ApplicationController
   end
 
   post '/users' do
-    user = User.find_by(params[:email_address])
+    user = User.find_by(email_address: params[:email_address])
     if user
-      
-      (first_name: params[first_name], last_name: params[:last_name], email_address: params[:email_address], password: params[:password], street_address: params[:street_address], city: params[:city], state: params[:state], zipcode: params[:zipcode] )
+      flash[:message] = 'Email address is already associated to an account, please login in'
+      redirect '/'
+    else
+      user = User.create(first_name: params[first_name], last_name: params[:last_name], email_address: params[:email_address], password: params[:password], street_address: params[:street_address], city: params[:city], state: params[:state], zipcode: params[:zipcode])
+      session[:id] = user.id
+      redirect '/'
+    end
+  end
 
+  patch '/users' do
+    user = Helpers.current_user(session)
+    if User.find_by(email_address: params[:email_address])
+      flash[:message] = 'Email is already associated to an account.'
+      redirect "/users/#{user.id}/edit"
+    else
+      user = User.update(params)
+      redirect '/'
+    end
   end
 
   get '/login' do
     erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(email_address: params[:email_address])
+    if user && user.authenticate(params[:password])
+      session[:user_id] == user.id
+      redirect '/'
+    else
+      flash[:message] = 'Incorrect email address or password, please try again.'
+      redirect '/login'
+    end
   end
 
   get '/users/:id' do
@@ -26,7 +52,7 @@ class UsersController < ApplicationController
   end
 
   get '/logout' do
-
+    session[:user_id].clear
   end
 
 end
