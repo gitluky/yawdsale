16bb4d47d135201062b1655d1a24aa18f55a8e59
Jonathan Luk <jluk87@gmail.com>
2019-02-11 19:09:19 -0500

    layout displays Log In and Sign Up when user is not logged in. User cannot view yawdsales when not logged in. Yawdsale show page updated

--- app/controllers/yawdsales_controller.rb
+++ app/controllers/yawdsales_controller.rb
@@ -1,33 +1,35 @@
 class YawdsalesController < ApplicationController
 
   get '/yawdsales' do
-    current_user = Helpers.current_user(session)
-    @map_string = "https://maps.googleapis.com/maps/api/staticmap?center=#{current_user.latitude},#{current_user.longitude}&zoom=#{zoom}&size=640x500"
-    @nearby_yawdsales = Yawdsale.near(current_user)
-    letter = "A"
-    @nearby_yawdsales.each do |yawdsale|
-      if yawdsale.end_time > DateTime.now
-        @map_string += "&markers=color:red%7Clabel:#{letter}%7C#{yawdsale.latitude}%2C#{yawdsale.longitude}"
-        letter = letter.next
+    if Helpers.logged_in?(session)
+      current_user = Helpers.current_user(session)
+      @map_string = "https://maps.googleapis.com/maps/api/staticmap?center=#{current_user.latitude},#{current_user.longitude}&zoom=#{zoom}&size=640x500"
+      @nearby_yawdsales = Yawdsale.near(current_user)
+      letter = "A"
+      @nearby_yawdsales.each do |yawdsale|
+        if yawdsale.end_time > DateTime.now
+          @map_string += "&markers=color:red%7Clabel:#{letter}%7C#{yawdsale.latitude}%2C#{yawdsale.longitude}"
+          letter = letter.next
+        end
       end
+      @map_string += "&key=#{File.read('./../apikey.txt')}"
+      erb :'/yawdsales/index'
+    else
+      redirect '/'
     end
-    @map_string += "&key=#{File.read('./../apikey.txt')}"
-    erb :'/yawdsales/index'
   end
 
   get '/yawdsales/search' do
     address = [params[:street_address], params[:city], params[:state], params[:zipcode]].compact.join(',')
     case params[:distance]
-    when 5
-      zoom=13
-    when 10
-      zoom=12
-    when 15
-      zoom=11
+      when 5
+        zoom=13
+      when 10
+        zoom=12
+      when 15
+        zoom=11
     end
-
     @map_string = "https://maps.googleapis.com/maps/api/staticmap?center=#{address.gsub(' ','+')}&zoom=#{zoom}&size=640x500"
-    binding.pry
     @search_results = Yawdsale.near(address, params[:distance])
     letter = "A"
     @search_results.each do |yawdsale|
