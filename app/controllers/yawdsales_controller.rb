e3af858a49b4eda9f88b24a0a8884fceee48d102
Jonathan Luk <jluk87@gmail.com>
2019-02-16 04:17:09 -0500

    user can add photos with the new yawdsale form

--- app/controllers/yawdsales_controller.rb
+++ app/controllers/yawdsales_controller.rb
@@ -22,11 +22,11 @@ class YawdsalesController < ApplicationController
   get '/yawdsales/search' do
     address = [params[:street_address], params[:city], params[:state], params[:zipcode]].compact.join(',')
     case params[:distance]
-      when 5
+      when "1"
         zoom=13
-      when 10
+      when "5"
         zoom=12
-      when 15
+      when "10"
         zoom=11
     end
     @map_string = "https://maps.googleapis.com/maps/api/staticmap?center=#{address.gsub(' ','+')}&zoom=#{zoom}&size=640x500"
@@ -58,9 +58,23 @@ class YawdsalesController < ApplicationController
       redirect "/yawdsales/new"
     else
       current_user = Helpers.current_user(session)
-      @yawdsale = current_user.yawdsales.create(params)
+      @yawdsale = Yawdsale.create(title: params[:title], description: params[:description], street_address: params[:street_address], city: params[:city], state: params[:state], zipcode: params[:zipcode], start_time: params[:start_time], end_time: params[:end_time])
+      @yawdsale.user = current_user
 
-      redirect "/yawdsales/#{@yawdsale.id}/photos/new"
+      path = "./public/#{@yawdsale.id}/photos"
+      FileUtils.mkdir_p path
+
+      params[:photos].each do |photo|
+        filename = photo[:filename]
+        file = photo[:tempfile]
+
+        File.write("#{path}/#{filename}", file.read)
+
+        @yawdsale.photos.create(filename: filename)
+
+      end
+      @yawdsale.save
+      redirect "/yawdsales/#{@yawdsale.id}"
     end
   end
 
@@ -78,62 +92,64 @@ class YawdsalesController < ApplicationController
     erb :'/yawdsales/edit'
   end
 
-  get '/yawdsales/:id/photos/new' do
-    @yawdsale = Yawdsale.find_by_id(params[:id])
-    erb :'/photos/new'
-  end
+  # get '/yawdsales/:id/photos/new' do
+  #   @yawdsale = Yawdsale.find_by_id(params[:id])
+  #   erb :'/photos/new'
+  # end
 
-  post '/yawdsales/:id/photos' do
+  # post '/yawdsales/:id/photos' do
+  #
+  #   current_user = Helpers.current_user(session)
+  #   yawdsale = Yawdsale.find_by_id(params[:id])
+  #   yawdsale_photos = Photo.where("yawdsale_id = #{yawdsale.id}")
+  #
+  #   if !yawdsale_photos.empty?
+  #     redirect "/yawdsales/#{yawdsale.id}"
+  #   end
+  #
+  #   path = "./public/#{yawdsale.id}/photos"
+  #   FileUtils.mkdir_p path
+  #
+  #   binding.pry
+  #
+  #   params[:photos].each do |photo|
+  #     filename = photo[:filename]
+  #     file = photo[:tempfile]
+  #
+  #     #File.open("./public/#{current_user}/#{filename}", 'wb') do |f|
+  #     File.write("#{path}/#{filename}", file.read)
+  #     #end
+  #
+  #     yawdsale.photos.create(filename: filename)
+  #   end
+  #     redirect "/yawdsales/#{params[:id]}"
+  # end
 
-    current_user = Helpers.current_user(session)
-    yawdsale = Yawdsale.find_by_id(params[:id])
-    yawdsale_photos = Photo.where("yawdsale_id = #{yawdsale.id}")
-
-    if !yawdsale_photos.empty?
-      redirect "/yawdsales/#{yawdsale.id}"
-    end
-
-    path = "./public/#{yawdsale.id}/photos"
-    FileUtils.mkdir_p path
-
-    params[:photos].each do |photo|
-      filename = photo[:filename]
-      file = photo[:tempfile]
-
-      #File.open("./public/#{current_user}/#{filename}", 'wb') do |f|
-      File.write("#{path}/#{filename}", file.read)
-      #end
-
-      yawdsale.photos.create(filename: filename)
-    end
-      redirect "/yawdsales/#{params[:id]}"
-  end
-
-  patch '/yawdsales/:id/photos' do
-    current_user = Helpers.current_user(session)
-    yawdsale = Yawdsale.find_by_id(params[:id])
-    yawdsale_photos = Photo.where("yawdsale_id = #{yawdsale.id}")
-    photo_count = yawdsale_photos.count || 0
-
-
-    params[:photos].each do |photo|
-      if photo_count < 3
-        filename = photo[:filename]
-        file = photo[:tempfile]
-
-        File.open("./public/#{yawdsale.id}/#{filename}", 'wb') do |f|
-           f.write(file.read)
-        end
-
-        current_user.photos.create(filename: filename)
-        photo_count +=1
-      else
-        flash[:message] = "There is a 3 photo limit per Yawdsale. Delete photos to add more."
-
-      end
-    end
-      redirect "/yawdsales/#{params[:id]}"
-  end
+  # patch '/yawdsales/:id/photos' do
+  #   current_user = Helpers.current_user(session)
+  #   yawdsale = Yawdsale.find_by_id(params[:id])
+  #   yawdsale_photos = Photo.where("yawdsale_id = #{yawdsale.id}")
+  #   photo_count = yawdsale_photos.count || 0
+  #
+  #
+  #   params[:photos].each do |photo|
+  #     if photo_count < 3
+  #       filename = photo[:filename]
+  #       file = photo[:tempfile]
+  #
+  #       File.open("./public/#{yawdsale.id}/#{filename}", 'wb') do |f|
+  #          f.write(file.read)
+  #       end
+  #
+  #       current_user.photos.create(filename: filename)
+  #       photo_count +=1
+  #     else
+  #       flash[:message] = "There is a 3 photo limit per Yawdsale. Delete photos to add more."
+  #
+  #     end
+  #   end
+  #     redirect "/yawdsales/#{params[:id]}"
+  # end
 
   get '/yawdsales/:id/photos/:photo_id' do
     @yawdsale = Yawdsale.find_by_id(params[:id])
@@ -143,12 +159,12 @@ class YawdsalesController < ApplicationController
     erb :'/photos/show_photo'
   end
 
-
-
-
-
   delete '/yawdsales/:id' do
     yawdsale = Yawdsale.find_by_id(params[:id])
+    path = "./public/#{yawdsale.id}"
+    if Dir.exists?(path)
+      FileUtils.remove_dir(path, force = false)
+    end
     if Helpers.current_user(session) == yawdsale.user
       Yawdsale.destroy(params[:id])
       redirect "/users/#{Helpers.current_user(session).id}"
